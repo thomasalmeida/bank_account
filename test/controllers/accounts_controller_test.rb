17527174733e2d7cf474ad7d55967575983e8b19
Thomnas Almeida <thomasfa86@hotmail.com>
2020-04-08 09:48:20 -0300

    Rename tests to be more readble

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -6,7 +6,7 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_equal JSON.parse(@response.body).keys, ['account_id', 'token']
   end
 
-  test 'create an invalid account with id duplicated' do
+  test 'not create an invalid account with id duplicated' do
     post signup_url(id: 'my_id', username: 'username', password: 'password', password_confirmation: 'password', balance: 5.0)
     post signup_url(id: 'my_id', username: 'username_1', password: 'password', password_confirmation: 'password', balance: 5.0)
 
@@ -14,7 +14,7 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_equal JSON.parse(@response.body)['errors'].pop, 'Id has already been taken'
   end
 
-  test 'create an invalid account with username duplicated' do
+  test 'not create an invalid account with username duplicated' do
     post signup_url(username: 'username', password: 'password', password_confirmation: 'password', balance: 5.0)
     post signup_url(username: 'username', password: 'password', password_confirmation: 'password', balance: 5.0)
 
