85b23c3e818e29d647ec80dbbf146b46518080a5
Thomnas Almeida <thomasfa86@hotmail.com>
2020-04-08 08:08:24 -0300

    Add Account model tests

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid account with id' do
+    account = Account.new(id: 'john', username: 'john', password: 'john', balance: 1.1)
+    assert account.valid?
+  end
+
+  test 'valid account without id' do
+    account = Account.new(username: 'john', password: 'john', balance: 1.0)
+    assert account.valid?
+  end
+
+  test 'valid account with id and balance' do
+    account = Account.new(username: 'john', password: 'john')
+    assert account.valid?
+  end
+
+  test 'valid account without balance' do
+    account = Account.new(id: 'john', username: 'john', password: 'john')
+    assert account.valid?
+  end
+
+  test 'invalid without username' do
+    account = Account.new(password: 'john')
+    refute account.valid?
+    assert_not_nil account.errors[:username]
+  end
+
+  test 'invalid without password' do
+    account = Account.new(username: 'john')
+    refute account.valid?
+    assert_not_nil account.errors[:password]
+  end
 end
